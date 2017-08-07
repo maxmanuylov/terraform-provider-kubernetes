41bbeaffe3d85bc4e2a3db7cde6f682be1c373b2
maxim.manuylov <maxim.manuylov@gmail.com>
2017-08-07 23:31:39 +0300

    fix hashicorp/go-plugin version

--- init.sh
+++ init.sh
@@ -8,5 +8,6 @@ govendor init
 
 govendor fetch github.com/maxmanuylov/go-rest/client
 govendor fetch github.com/maxmanuylov/utils/http/transport/tls
+govendor fetch github.com/hashicorp/go-plugin@f72692aebca2008343a9deb06ddb4b17f7051c15
 govendor fetch github.com/hashicorp/terraform@=$TERRAFORM_VERSION
 govendor fetch github.com/maxmanuylov/utils/intellij-hcl/terraform/provider-schema-generator@=v2
